ac6db63bbf83f456c7a7712dbee37773cb5ddbf2
Simon Greene <simongreene02@gmail.com>
2016-04-09 10:06:56 -0500

    Implemented isEmptyReigon

--- Misc/src/practice/exam4/problem4/FloorPlan.java
+++ Misc/src/practice/exam4/problem4/FloorPlan.java
@@ -12,9 +12,44 @@ public class FloorPlan {
 		COLS = cols;
 		room = new int[ROWS][COLS];
 	}
+	
+	FloorPlan(int[][] room) {
+		if (room.length == 0) {
+			throw new IllegalArgumentException("room must greater than 0");
+		}
+		this.room = room;
+		ROWS = room.length;
+		COLS = room[0].length;
+	}
 
-	private boolean isEmptyRegion(int top, int bottom, int left, int right) {
-		throw new UnsupportedOperationException("Not Implemented Yet");
+	boolean isEmptyRegion(final int top, final int bottom, final int left, final int right) {
+		int trueTop = top;
+		int trueBottom = bottom;
+		int trueLeft = left;
+		int trueRight = right;
+		if (trueTop > 0) {
+			trueTop--;
+		}
+		if (trueBottom < ROWS - 1) {
+			trueBottom++;
+		}
+		if (trueLeft > 0) {
+			trueLeft--;
+		}
+		if (trueRight < COLS - 1) {
+			trueRight++;
+		}
+		for (int row = trueTop; row <= trueBottom; row++) {
+			for (int col = trueLeft; col <= trueRight; col++) {
+				System.out.print(row);
+				if (room[row][col] == 1) {
+					return false;
+				}
+			}
+			System.out.println();
+		}
+		System.out.println("=======================================");
+		return true;
 	}
 
 	public boolean fits(int width, int height, Location ulCorner) {
--- Misc/src/practice/exam4/problem4/FloorPlanTest.java
+++ Misc/src/practice/exam4/problem4/FloorPlanTest.java
@@ -6,9 +6,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class FloorPlanTest {
-
+	private FloorPlan floorPlan;
 	@Before
 	public void setUp() throws Exception {
+		int [][] data = new int[5][5];
+		data[0] = new int[]{0,0,1,0,0};
+		data[0] = new int[]{0,1,0,1,0};
+		data[0] = new int[]{0,1,0,1,0};
+		data[0] = new int[]{1,0,0,0,1};
+		data[0] = new int[]{1,0,0,0,1};
+
+		floorPlan = new FloorPlan(data);
 	}
 
 	@Test
@@ -20,6 +28,44 @@ public class FloorPlanTest {
 				assertEquals(0, room[row][col]);
 			}
 		}
+	}
+
+	@Test
+	public void isEmptyRegion_allSides() {
+		FloorPlan floorPlan = new FloorPlan(8, 9);
+		int[][] room = floorPlan.getRoom();
+		assertTrue(floorPlan.isEmptyRegion(2, 3, 2, 5));
+	}
+
+	@Test
+	public void isEmptyRegion_topRightBottom() {
+		FloorPlan floorPlan = new FloorPlan(8, 9);
+		int[][] room = floorPlan.getRoom();
+		assertTrue(floorPlan.isEmptyRegion(2, 3, 0, 3));
+	}
+
+	@Test
+	public void isEmptyRegion_topRight() {
+		FloorPlan floorPlan = new FloorPlan(8, 9);
+		int[][] room = floorPlan.getRoom();
+		assertTrue(floorPlan.isEmptyRegion(6, 7, 0, 3));
+	}
+
+	@Test
+	public void isEmptyRegion_topLeftRight() {
+		FloorPlan floorPlan = new FloorPlan(8, 9);
+		int[][] room = floorPlan.getRoom();
+		assertTrue(floorPlan.isEmptyRegion(7, 8, 1, 4));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void initalization_empty() {
+		new FloorPlan(new int[0][0]);
+	}
+	
+	@Test
+	public void isEmptyRegion_topRightBottomWithOnes() {
+		assertFalse(floorPlan.isEmptyRegion(2, 3, 0, 3));
 	}
 
 }
