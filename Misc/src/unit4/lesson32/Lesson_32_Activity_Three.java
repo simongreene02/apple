2c51fe4995b8c91f52228db856acc71631af392d
Simon Greene <simongreene02@gmail.com>
2016-04-30 11:07:15 -0500

    Finished Activity 3

---
+++ Misc/src/unit4/lesson32/Lesson_32_Activity_Three.java
@@ -0,0 +1,10 @@
+package unit4.lesson32;
+
+public class Lesson_32_Activity_Three {
+	public static String trueSwap(int a, int b) {
+		return b + " " + a;
+	}
+	public static void swap(int a, int b) {
+		System.out.println(trueSwap(a, b));
+	}
+}
---
+++ Misc/src/unit4/lesson32/Lesson_32_Activity_ThreeTest.java
@@ -0,0 +1,16 @@
+package unit4.lesson32;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Lesson_32_Activity_ThreeTest {
+
+
+	@Test
+	public void swap() {
+		assertEquals("8 7", Lesson_32_Activity_Three.trueSwap(7, 8));
+	}
+
+}
