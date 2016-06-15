2acb7e127df05edbde42e4cd03882d118d93508c
Simon Greene <simongreene02@gmail.com>
2016-06-15 12:36:49 -0500

    Finished the coding activity for unit 5 lesson 1

---
+++ Misc/src/unit5/lesson1/activity1/Main.java
@@ -0,0 +1,26 @@
+package unit5.lesson1.activity1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter words, enter STOP to stop the loop.");
+		String input = scanner.next();
+		ArrayList<String> outputList = new ArrayList<String>();
+		while (!input.equals("STOP")) {
+			outputList.add(input);
+			input = scanner.next();
+		}
+		System.out.println(outputList.size());
+		System.out.println(outputList);
+		if (outputList.size() > 2) {
+			outputList.remove(0);
+			outputList.remove(outputList.size() - 1);
+			System.out.println(outputList);
+		}
+	}
+
+}
